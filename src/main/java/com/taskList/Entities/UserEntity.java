afa11b6dbfd9f20628699082f68ebc1e468755a1
Axel <axelpartida584@gmail.com>
2025-04-18 23:06:30 -0600

    Implementando el repositorio de usuarios de forma correcta, viene de la rama implementarUserRepo

--- src/main/java/com/taskList/Entities/UserEntity.java
+++ src/main/java/com/taskList/Entities/UserEntity.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/taskList/Repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.taskList.Repository;
+
+import com.taskList.Entities.TaskEntity;
+import com.taskList.Entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+//Clase para poder hacer consultas a la BD regresando objetos del tipo Usuario, verificar si está registrado o no.
+@Repository
+public interface UserRepository extends JpaRepository<TaskEntity, Long> {
+    Optional<UserEntity> findByUsername(String username); //Encontrar usuarios por el nombre de usuario.
+}
