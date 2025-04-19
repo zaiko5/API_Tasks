0f74140046c94eca21b1a44035ade374512dccf3
Axel <axelpartida584@gmail.com>
2025-04-18 23:01:23 -0600

    Implementar la entidad usuario correctamente en la BD

---
+++ src/main/java/com/taskList/Entities/User.java
@@ -0,0 +1,24 @@
+package com.taskList.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Clase que va a definir el comportamiento de un objeto usuario en la BD, no necesita relacion con las tareas, ya que aqui solo se guardará informacion para las credenciales del usuario.
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    private String rol; //"USER" o "ADMIN"
+}
